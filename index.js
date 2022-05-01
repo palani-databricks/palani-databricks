f0c69b7f5a628c7ffd12a21f390bf9d00d92f177
palani-databricks <104568706+palani-databricks@users.noreply.github.com>
2022-05-01 16:59:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-import React from 'react';
-import {Helmet} from 'react-helmet';
-
-class Application extends React.Component {
-  render () {
-    return (
-        <div className="application">
-            <Helmet>
-                <style>{'body { background-color: red; }'}</style>
-            </Helmet>
-            ...
-        </div>
-    );
-  }
-};
-
 var express = require('express');
 var app = express();
 
